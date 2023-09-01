43ad97b87d7ac6616f40f20a22589992d53f9b1e
oscar-fernandez <68928859+oscar-fernandez@users.noreply.github.com>
2023-08-31 21:24:49 -0400

    Add Layout and Sidebar components. Style Sidebar

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import Sidebar from '../Sidebar/Sidebar';
+import './Layout.scss';
+
+const Layout = () => {
+  return (
+    <>
+      <div>Hello</div>
+      <Sidebar />
+    </>
+  );
+};
+
+export default Layout;
