f5f11d606d18bd3fd520ffab3abf1dfcc3ce3e83
oscar-fernandez <68928859+oscar-fernandez@users.noreply.github.com>
2023-09-04 12:54:11 -0400

    Add and style AnimatedLetters

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,56 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/images/o-logo.png';
 import './Home.scss';
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = ['s', 'c', 'a', 'r'];
+  const jobArray = [
+    'W',
+    'e',
+    'b',
+    ' ',
+    'D',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+  ];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+  });
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <span className="tag tag-top">&lt;h1&gt;</span>
         <h1>
-          Hi, <br /> I'm
-          <img src={Logo} alt="developer" />
-          scar
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
           <br />
-          Web Developer
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={Logo} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={15}
+          />
         </h1>
         <span className="tag tag-bottom">&lt;/h1&gt;</span>
         <h2>Fullstack Developer / IT Professional</h2>
